68e1a88f76017db050f317a44d376c56a2efdb88
Dhruba2004 <dhrubaduttabanik8@gmail.com>
2024-03-03 09:35:58 +0530

    feat : hover effect added

--- Js/symptomchecker.js
+++ Js/symptomchecker.js
@@ -1,12 +1,42 @@
-document.getElementById('rotateBtn').addEventListener('click', function() {
-    let frontImage = document.getElementById('frontImage');
-    let backImage = document.getElementById('backImage');
+const bodyParts = [
+  { id: "headSvg", divId: "headDiv" },
+  { id: "neckSvg", divId: "neckDiv"},
+  { id: "leftarmSvg", divId: "armDiv"},
+  { id: "rightarmSvg", divId: "armDiv" },
+  { id: "leftlegSvg", divId: "legDiv"},
+  { id: "rightlegSvg", divId: "legDiv"},
+  { id: "chestSvg", divId: "chestDiv"},
+  { id: "AbdomenSvg", divId: "AbdomenDiv"},
+  { id: "pelvisSvg", divId: "pelvisDiv"}
+];
 
-    if (frontImage.style.display === 'none') {
-      frontImage.style.display = 'block';
-      backImage.style.display = 'none';
-    } else {
-      frontImage.style.display = 'none';
-      backImage.style.display = 'block';
-    }
-  });
+bodyParts.forEach(bodyPart => {
+  const svg = document.getElementById(bodyPart.id);
+
+  svg.addEventListener("mouseenter", () => showBodyPart(svg));
+  svg.addEventListener("mouseleave", () => hideBodyPart(svg));
+});
+
+function showBodyPart(svg) {
+  svg.style.opacity = "1";
+}
+
+function hideBodyPart(svg) {
+  svg.style.opacity = "0";
+
+}
+
+
+
+document.getElementById("rotateBtn").addEventListener("click", function () {
+  let frontImage = document.getElementById("frontImage");
+  let backImage = document.getElementById("backImage");
+
+  if (frontImage.style.display === "none") {
+    frontImage.style.display = "block";
+    backImage.style.display = "none";
+  } else {
+    frontImage.style.display = "none";
+    backImage.style.display = "block";
+  }
+});
